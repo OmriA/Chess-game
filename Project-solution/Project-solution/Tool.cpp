0e2321d9283b1f05650e78b86f043d82f619f0e0
ItayGirstein <itayGirstein@gmail.com>
2017-12-12 17:46:22 +0200

    finished forward func

--- Project-solution/Project-solution/Tool.cpp
+++ Project-solution/Project-solution/Tool.cpp
@@ -1,15 +1,25 @@
 #include "Tool.h"
 #include "Empty.h"
 
+/**
+Checking if the tool can move forward or backwards.
+Input:	board - the board
+		turn - whose turn
+		src - the source location
+		dst - the destination location
+Output:	True if the tool can move, False if not.
+**/
 bool forward(Board board, bool turn, Location src, Location dst)
 {
 	if (board.getIndex(src.getRow(), src.getCol())->getColor() == turn)	//checking if the tool is the player's tool
 	{
-		if (board.getIndex(dst.getRow(), dst.getCol())->getColor() != turn)	//checking if dst has no tool from player's tools
+		if ((board.getIndex(dst.getRow(), dst.getCol())->getSign() != '#'	//checking if dst is a tool
+			&& board.getIndex(dst.getRow(), dst.getCol())->getColor() != turn)	//checking if dst tool isn't from the player's tools
+			|| (board.getIndex(dst.getRow(), dst.getCol())->getSign() == '#'))	//checking if dst has no tool
 		{
 			if (src.getRow() > dst.getRow())
 			{
-				for (unsigned int i = src.getRow() - 1; i > dst.getRow(); i--)
+				for (unsigned int i = src.getRow() - 1; i > dst.getRow(); i--)	//checking if there is any tool between src to dst
 				{
 					if (board.getIndex(i, src.getCol())->getSign() != '#')
 					{
@@ -19,16 +29,16 @@ bool forward(Board board, bool turn, Location src, Location dst)
 			}
 			else
 			{
-				for (unsigned int i = src.getRow() + 1; i < dst.getRow(); i++)
+				for (unsigned int i = src.getRow() + 1; i < dst.getRow(); i++)	//checking if there is any tool between src to dst
 				{
 					if (board.getIndex(i, src.getCol())->getSign() != '#')
 					{
 						return false;
 					}
 				}
-			}
+			}	//end of else
+			return true;
 		}
-		return true;
 	}
 	return false;
 }
