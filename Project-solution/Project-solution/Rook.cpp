a55038ccd6432b95e844ed222c095b0e25476de1
OmriA <omriabramovich@gmail.com>
2017-12-15 15:05:26 +0200

    added project files (pipe, etc.)

--- Project-solution/Project-solution/Rook.cpp
+++ Project-solution/Project-solution/Rook.cpp
@@ -7,10 +7,10 @@ int Rook::isLegal(Board board, int turn, Location src, Location dst)
 	int flag = 0;
 	if (src.getCol() == dst.getCol())	//checking if the src and the dest are vertical.
 	{
-		_moves["vertical"](board, turn, src, dst);
+		flag = _moves["vertical"](board, turn, src, dst);
 	}
 	else if (src.getRow() == dst.getRow())	//checking if the src and the dest are horizontal.
 	{
-		_moves["horizontal"](board, turn, src, dst);
+		flag = _moves["horizontal"](board, turn, src, dst);
 	}
 }
